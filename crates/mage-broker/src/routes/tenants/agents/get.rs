feb5b5783918daf4c786c45667327c29a0e438c4
aacebo <aacebowork@gmail.com>
2026-08-06 15:24:12 -0400

    Update get.rs

--- crates/mage-broker/src/routes/tenants/agents/get.rs
+++ crates/mage-broker/src/routes/tenants/agents/get.rs
@@ -1,6 +1,7 @@
 use axum::extract::{Path, Query};
 use axum::response::{IntoResponse, Response};
 use mage_error::Result;
+use mage_types::actors::Role;
 
 use crate::state;
 
@@ -9,6 +10,10 @@ pub async fn get(
     Path(tenant_id): Path<uuid::Uuid>,
     Query(query): Query<mage_storage::actors::Query>,
 ) -> Result<Response> {
-    let actors = session.storage().actors().get(query.tenant(tenant_id)).await?;
+    let actors = session
+        .storage()
+        .actors()
+        .get(query.tenant(tenant_id).roles(vec![Role::Agent]))
+        .await?;
     Ok(axum::Json(actors).into_response())
 }
